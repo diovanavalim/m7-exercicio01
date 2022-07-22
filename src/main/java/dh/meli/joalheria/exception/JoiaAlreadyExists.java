8123ed51a88908a27ab8037983265eddfd47ad97
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-22 14:14:48 -0300

    feat: add respose status annotation

--- src/main/java/dh/meli/joalheria/exception/JoiaAlreadyExists.java
+++ src/main/java/dh/meli/joalheria/exception/JoiaAlreadyExists.java
@@ -1,5 +1,9 @@
 package dh.meli.joalheria.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class JoiaAlreadyExists extends RuntimeException{
     public JoiaAlreadyExists(String message) {
         super(message);
--- src/main/java/dh/meli/joalheria/exception/JoiaNotFound.java
+++ src/main/java/dh/meli/joalheria/exception/JoiaNotFound.java
@@ -1,5 +1,9 @@
 package dh.meli.joalheria.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class JoiaNotFound extends RuntimeException {
     public JoiaNotFound (String message) {
         super(message);
