43e99b1ac9d14c12f272b48afc59952e2ae3d5e1
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-22 16:09:47 -0300

    fix: remove unused import

--- src/main/java/dh/meli/joalheria/service/JoiaService.java
+++ src/main/java/dh/meli/joalheria/service/JoiaService.java
@@ -8,7 +8,6 @@ import dh.meli.joalheria.repository.IJoiaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
